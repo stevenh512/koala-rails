6be6820d93a59d5806e1bd1ee943968dd9291db2
Steven Hancock <stevenh512@gmail.com>
2010-06-21 18:37:11 -0700

    Updated helpers

--- lib/koala-rails/helpers.rb
+++ lib/koala-rails/helpers.rb
@@ -1,11 +1,19 @@
 module KoalaRails
   module Helpers
-    def facebook_cookies
-      @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_from_cookie(cookies)
+    def facebook_session
+      @facebook_session ||= Koala::Facebook::OAuth.new.get_user_from_cookie(cookies)
     end
 
     def facebook_token
-      @facebook_token ||= facebook_cookies['access_token']
+      @facebook_access_token ||= facebook_session['access_token']
+    end
+
+    def facebook_uid
+      @facebook_uid ||= facebook_session['uid']
+    end
+
+    def facebook_session_key
+      @facebook_session_key ||= facebook_session['session_key']
     end
   end
 end
